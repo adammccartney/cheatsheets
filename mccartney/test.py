219478ae6e26dc7ed604d32ca89e570a0d549052
adammccartney <adam@mur.at>
2020-07-09 19:12:09 +0200

    added some actual tests ;-)

--- mccartney/test.py
+++ mccartney/test.py
@@ -1,1 +1,7 @@
-pass
+import markups
+
+markup = markups.instrument('Violin')
+print("Violin markup: ", markup)
+
+short_instrument_markup = markups.short_instrument('Vln.')
+print("Short markup: ", short_instrument_markup)
