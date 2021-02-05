6586bd18a5112b9b53813112b1c100744aa4ab76
adammccartney <adam@mur.at>
2021-02-05 14:44:33 +0100

    update for backup

--- mccartney/test.py
+++
@@ -1,7 +0,0 @@
-import markups
-
-markup = markups.instrument('Violin')
-print("Violin markup: ", markup)
-
-short_instrument_markup = markups.short_instrument('Vln.')
-print("Short markup: ", short_instrument_markup)
